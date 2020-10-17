42889694bb3756af176cc9737fc7c944212a3839
Randall Svancara <rsvancara@gmail.com>
2020-10-17 17:19:01 +0100

    file upload works

--- sounder-gong.go
+++ sounder-gong.go
@@ -10,10 +10,18 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"io"
 	"os/exec"
 	"os/signal"
 	"time"
 )
+
+type Song struct {
+	ID  string `json:"id"`
+	Title string `json:"title"`
+	Description string `json:"description"`
+	Path string `json:"path"`
+}
 
 func main() {
 
@@ -25,7 +33,7 @@ func main() {
 	r := mux.NewRouter()
 	r.Handle("/", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(HomeHandler)))
 	r.Handle("/add", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(AddHandler)))
-	r.Handle("/delete/{soundid}", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(DeleteHandler))) 
+	r.Handle("/delete/{soundid}", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(DeleteHandler)))
 	r.Handle("/play/{soundid}", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(PlaySoundHandler)))
 
 	dir := "./static" 
@@ -93,7 +101,86 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 // AddHandler handler for adding songs
 func AddHandler(w http.ResponseWriter, r *http.Request) {
-	//vars := mux.Vars(r)
+
+	titleMessage := ""
+	titleMessageError := false
+	descriptionMessage := ""
+	descriptionMessageError := false
+	fileMessage := ""
+	fileMessageError := false
+	var song Song
+
+	if r.Method == http.MethodPost {
+
+		validate := true
+		if err := r.ParseMultipartForm(10 << 20); nil != err {
+			validate = false
+			fileMessage = fmt.Sprintf("Error uploading file: %s",err)
+			fileMessageError = true
+			fmt.Println("Error Retrieving the File")
+		}
+
+		song.Title = r.FormValue("inputTitle")
+		song.Description = r.FormValue("inputDescription")
+
+		// Validation
+		if song.Title == "" {
+			validate = false
+			titleMessage =  "Please provide a title"
+			titleMessageError = true
+		}
+
+
+		if song.Description == "" {
+			validate = false
+			descriptionMessage = "Please provide a title"
+			descriptionMessageError = true
+		}
+
+		// Get handler for filename, size and headers
+		file, handler, fileErr := r.FormFile("inputFile")
+		if fileErr != nil {
+
+			validate = false
+			fileMessage = fmt.Sprintf("Error uploading file: %s",fileErr)
+			fileMessageError = true
+			fmt.Println("Error Retrieving the File")
+			fmt.Println(fileErr)
+
+		}
+
+
+		if validate == true {
+
+			fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+			fmt.Printf("File Size: %+v\n", handler.Size)
+			fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+			// Create file
+			dst, err := os.Create(handler.Filename)
+			defer dst.Close()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			// Copy the uploaded file to the created file on the filesystem
+			if _, err := io.Copy(dst, file); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			//fmt.Fprintf(w, "Successfully Uploaded File\n")
+
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+
+		}
+
+		if fileErr == nil {
+			file.Close()
+		}
+	}
 
 	template := "templates/add.html"
 	//template := "templates/index.html"
@@ -101,6 +188,13 @@ func AddHandler(w http.ResponseWriter, r *http.Request) {
 
 	out, err := tmpl.Execute(pongo2.Context{
 		"title":     "Add",
+		"titleMessage": titleMessage,
+		"titleMessageError": titleMessageError,
+		"descriptionMessage": descriptionMessage,
+		"descriptionMessageError": descriptionMessageError,
+		"fileMessage": fileMessage,
+		"fileMessageError": fileMessageError,
+
 	})
 
 	if err != nil {
@@ -141,4 +235,38 @@ func PlaySound(soundfile string) {
 	}
 
 	fmt.Printf("%s\n", out)
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	// Maximum upload of 10 MB files
+	r.ParseMultipartForm(10 << 20)
+
+	// Get handler for filename, size and headers
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	// Create file
+	dst, err := os.Create(handler.Filename)
+	defer dst.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Copy the uploaded file to the created file on the filesystem
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
 }
